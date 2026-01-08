1ffe3550e5ae40668fb6af4a4568b386b599e185
Siryk Oleksandr <volgabasss@gmail.com>
2026-01-09 00:48:27 +0200

    added rag system for long-term for big chats

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import llm_model, system_prompt
 from db import save_message_db, get_chat_history, chat_reset
+from vector_db import save_interaction_embedding, get_memory_block
 
 app = FastAPI()
 #creates llm_model instance that stores llm model created using llama-cpp-python and settings from config.json
@@ -29,12 +30,16 @@ app.add_middleware(
 #chat completition endpoint
 @app.post('/chat')
 def chat(user_data: UserInput):
-    #concatenating system prompt from backend with system prompt from frontend
-    final_system_prompt = user_data.user_system_prompt + system_prompt
+    user_query = user_data.message
+    #getting rag memory
+    rag_db_memory = get_memory_block(user_query)
+    #concatenating system prompt from backend with system prompt from frontend and context from rag_db
+    final_system_prompt = f'{user_data.user_system_prompt} \n\n{system_prompt} \n\n{rag_db_memory}'
+    now_timestamp = datetime.now().timestamp()
     #creating context with system prompt
     context = [{"role":"system", "content":final_system_prompt, "timestamp": datetime.now().timestamp()}]
     #saving last user message to db
-    save_message_db(msg=user_data.message, role='user')
+    save_message_db(msg=user_query, role='user')
     #getting chat history from db in the form of the list of dictionaries
     history = get_chat_history()
     #extending context with chat history from db
@@ -44,7 +49,8 @@ def chat(user_data: UserInput):
     parsed_json = json.loads(json_string, strict=False)
     response = parsed_json.get("response", "")
     save_message_db(msg=response, role='assistant')
-    response_dict = {"role":"assistant","content": response, "timestamp":datetime.now().timestamp()}
+    save_interaction_embedding(user_msg=user_query, assistant_msg=response)
+    response_dict = {"role":"assistant","content": response, "timestamp":now_timestamp}
     context.append(response_dict)
     return response_dict
 
@@ -55,6 +61,6 @@ def get_history():
 @app.get('/reset')
 def reset_chat():
     chat_reset()
-    
+
 if __name__ == '__main__':
     uvicorn.run(app, host='localhost', port=8000)
---
+++ vector_db.py
@@ -0,0 +1,53 @@
+import chromadb
+import uuid
+from datetime import datetime
+
+#simple RAG system that will provide some long-term memory for the model outside of the scope of context window, also should provide emphasis on the similar queries inside the context window
+
+#initializing client, path is chroma by default, sqlite as db provider
+#default embedding model for chroma is MiniLM-L6-v2
+client = chromadb.PersistentClient()
+
+collection = client.get_or_create_collection(name="chat_history")
+
+#adding interaction to db
+def save_interaction_embedding(user_msg:str, assistant_msg:str):
+    now = datetime.now()
+    #timestamp in the float format for metadata
+    timestamp_float = now.timestamp()
+    #timestampt for the interaction content in the format Y-M-D H:M
+    datetime_string = now.strftime('%Y-%m-%d %H:%M')
+    collection.add(
+    documents=[f'[{datetime_string}] user: {user_msg} \nassistant:{assistant_msg}'],
+    metadatas=[{'timestamp': timestamp_float, 'hsnw:space':'cosine'}],
+    ids=[str(uuid.uuid4())]
+)
+
+#searching similar messages to the query using cosine similarity
+def get_string_vector(query, res_count=2):
+    full_text = ''
+    results = collection.query(
+        query_texts=[query],
+        n_results=res_count
+    )
+    interactions = results['documents'][0]
+    distances = results['distances'][0]
+    for (doc, dist) in zip(interactions, distances):
+        if(dist<=0.7):
+            full_text += doc + '\n\n'
+    full_text = full_text.strip()
+    return full_text
+
+def get_memory_block(query, res_count=2):
+    rag_content = get_string_vector(query, res_count)
+    memory_block = ''
+    print(rag_content)
+    if rag_content:
+        memory_block = f"""
+        <relevant_memory>
+            The following are excerpts from past conversations. Use them for context.
+            DATA:
+            {rag_content}
+        </relevant_memory>
+        """
+    return memory_block
